bdaad9e4bf7d54b0e7686a80e3b6e102f1ffb603
Karan <karan@karan.com>
2024-02-09 20:47:59 +0000

    typo fixed

--- pipeline/build/mvn.sh
+++ pipeline/build/mvn.sh
@@ -8,4 +8,4 @@ WORKSPACE=/home/ubuntu/demo/jenkins_home/workspace/demo
 
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 
-cp java-app/target/*.war pipleine/build/
+cp java-app/target/*.war pipeline/build/
