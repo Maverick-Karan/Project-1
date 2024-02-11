bc57f13ed924a0c305e0179e7b9bda8f2018c839
Ubuntu <ubuntu@ip-172-31-29-103.ec2.internal>
2024-02-11 20:19:47 +0000

    added chmod in deploy.sh

--- pipeline/deploy/deploy.sh
+++ pipeline/deploy/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo chmod 600 pipeline/deploy/key
+
 scp -i pipeline/deploy/key pipeline/deploy/publish.sh ubuntu@prod:/home/ubuntu/
 scp -i pipeline/deploy/key pipeline/deploy/docker-compose.yml ubuntu@prod:/home/ubuntu/
 
