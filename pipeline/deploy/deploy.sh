30b8b3d998a3c89be10e38b731c138dfb733d041
Karan <karan@karan.com>
2024-02-11 00:07:47 +0000

    changed prod permissions

--- pipeline/deploy/deploy.sh
+++ pipeline/deploy/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-scp -i pipeline/deploy/prod pipeline/deploy/publish.sh ubuntu@prod:/home/ubuntu/
-scp -i pipeline/deploy/prod pipeline/deploy/docker-compose.yml ubuntu@prod:/home/ubuntu/
+scp -i pipeline/deploy/prod-key pipeline/deploy/publish.sh ubuntu@prod:/home/ubuntu/
+scp -i pipeline/deploy/prod-key pipeline/deploy/docker-compose.yml ubuntu@prod:/home/ubuntu/
 
-ssh -i pipeline/deploy/prod ubuntu@prod "export IMAGE=$IMAGE"
-ssh -i pipeline/deploy/prod ubuntu@prod "./home/ubuntu/publish.sh"
+ssh -i pipeline/deploy/prod-key ubuntu@prod "export IMAGE=$IMAGE"
+ssh -i pipeline/deploy/prod-key ubuntu@prod "./home/ubuntu/publish.sh"
