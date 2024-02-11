b0f4846d008b41948f1f399bf271d2ba8c4084b9
Ubuntu <ubuntu@ip-172-31-29-103.ec2.internal>
2024-02-11 20:52:00 +0000

    changed to docker-compose-prod

--- pipeline/deploy/deploy.sh
+++ pipeline/deploy/deploy.sh
@@ -3,7 +3,7 @@
 chmod 600 pipeline/deploy/key
 
 scp -i pipeline/deploy/key pipeline/deploy/publish.sh ubuntu@prod:/home/ubuntu/
-scp -i pipeline/deploy/key pipeline/deploy/docker-compose.yml ubuntu@prod:/home/ubuntu/
+scp -i pipeline/deploy/key pipeline/deploy/docker-compose-prod.yml ubuntu@prod:/home/ubuntu/
 
 ssh -i pipeline/deploy/key ubuntu@prod "export IMAGE=$IMAGE"
-ssh -i pipeline/deploy/key ubuntu@prod "./home/ubuntu/publish.sh"
+ssh -i pipeline/deploy/key ubuntu@prod "./publish.sh"
