711d506365f28e83059284ba306315a267175b5e
Ubuntu <ubuntu@ip-172-31-29-103.ec2.internal>
2024-02-11 21:06:06 +0000

    set variables

--- pipeline/deploy/deploy.sh
+++ pipeline/deploy/deploy.sh
@@ -5,5 +5,5 @@ chmod 600 pipeline/deploy/key
 scp -i pipeline/deploy/key pipeline/deploy/publish.sh ubuntu@prod:/home/ubuntu/
 scp -i pipeline/deploy/key pipeline/deploy/docker-compose-prod.yml ubuntu@prod:/home/ubuntu/
 
-ssh -i pipeline/deploy/key ubuntu@prod "export IMAGE=$IMAGE"
+ssh -i pipeline/deploy/key ubuntu@prod "export IMAGE=${IMAGE} BUILD_NUMBER=${BUILD_NUMBER}"
 ssh -i pipeline/deploy/key ubuntu@prod "./publish.sh"
