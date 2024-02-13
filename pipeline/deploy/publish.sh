f934669844c655ba4b5955cccbecf68f57bf7733
Ubuntu <ubuntu@ip-172-31-29-103.ec2.internal>
2024-02-13 02:39:57 +0000

    port remapped

--- pipeline/deploy/publish.sh
+++ pipeline/deploy/publish.sh
@@ -3,6 +3,6 @@
 export BUILD_NUMBER=$(sed -n '1p' /tmp/build.txt)
 export IMAGE=$(sed -n '2p' /tmp/build.txt)
 
-docker login -u maverick8266 -p PASS
+docker login -u maverick8266 -p $PASS
 
 cd /home/ubuntu/ && docker-compose -f docker-compose-prod.yml up -d
